df102c91c78a87ebb946eb544498afd56afabf7a
Veltin Dupont <vdupont@octo.com>
2016-09-24 10:37:54 +0200

    Adding the blackbox

--- src/physics/train.py
+++ src/physics/train.py
@@ -1,13 +1,19 @@
+from src.physics.record import BlackBox
+
+
 class Train:
-    def __init__(self, initial_position, initial_speed, controler):
+    def __init__(self, initial_position, initial_speed, controler, black_box=None):
         self.position = initial_position
         self.speed = initial_speed
+        self.max_brake_strength = 10
         self.controler = controler
         self.controler.set_train(self)
-        self.max_brake_strength = 10
+        self.black_box = black_box or BlackBox()
+        self.black_box.record(self.position, self.speed)
 
     def move(self, duration):
         self._update_position_and_speed(duration)
+        self.black_box.record(self.position, self.speed)
 
     def _update_position_and_speed(self, duration):
         self._update_position(duration)
