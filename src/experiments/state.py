2ec3b5fe1dace6a6c9fd7ecae246a50a7eba241d
Veltin Dupont <vdupont@octo.com>
2016-09-25 22:28:24 +0200

    Adding PreviousState to Transitions

--- src/experiments/state.py
+++ src/experiments/state.py
@@ -8,6 +8,16 @@ class State:
                                                                   speed=self.speed)
 
 
+class PreviousState:
+    def __init__(self, episode_step):
+        self.position = episode_step.train.black_box.position_record[-2]
+        self.speed = episode_step.train.black_box.speed_record[-2]
+
+    def __repr__(self):
+        return "State(Position {position}, Speed {speed})".format(position=self.position,
+                                                                  speed=self.speed)
+
+
 def compute_reward(episode_step):
     distance = episode_step.train.controler.objective - episode_step.train.position
     if abs(distance) < 1:
--- src/experiments/transitions.py
+++ src/experiments/transitions.py
@@ -1,4 +1,4 @@
-from src.experiments.state import State
+from src.experiments.state import State, PreviousState
 
 
 class Transition:
@@ -6,14 +6,17 @@ class Transition:
         self.reward = compute_reward(episode_step)
         self.previous_brake_power = episode_step.train.black_box.braking_record[-1]
         self.state = State(episode_step)
+        self.previous_state = PreviousState(episode_step)
 
     def save(self, record):
         record.save_transition(self)
 
     def __repr__(self):
-        return "Transition(Reward {reward}, Brake {brake}, {state})".format(reward=self.reward,
-                                                                            brake=self.previous_brake_power,
-                                                                            state=self.state)
+        return "Transition(Reward {reward}, Brake {brake}, " \
+               "{state}, {previous_state})".format(reward=self.reward,
+                                                   brake=self.previous_brake_power,
+                                                   state=self.state,
+                                                   previous_state=self.previous_state)
 
 
 def compute_reward(episode_step):
