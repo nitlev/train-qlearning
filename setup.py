aad43fed234d46d4993e168c8e7326fdf40cf556
Veltin Dupont <vdupont@octo.com>
2016-09-27 11:43:44 +0200

    Updating config files

--- setup.py
+++ setup.py
@@ -22,18 +22,32 @@ def read(*names, **kwargs):
 
 setup(
     name='trainqlearning',
+    url="https://github.com/nitlev/pyzmq-helloworld",
+    author='Veltin DUPONT',
+    author_email='veltind@gmail.com',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: CI Tools',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='deep-learning reinforcement-learning train',
     version='0.1.0',
     license='BSD',
     description='This is a toy reinforcement learning project',
     long_description=re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.md')),
-    author='Ionel Cristian Mărieș',
-    author_email='contact@ionelmc.ro',
-    url='https://github.com/ionelmc/python-nameless',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     py_modules=[splitext(basename(i))[0] for i in glob.glob("src/*.py")],
-    keywords=[
-    ],
     install_requires=[
     ],
     extras_require={
