f8c721e3932823d5bc6374ea4453787df04754e7
Veltin Dupont <vdupont@octo.com>
2016-10-02 21:25:29 +0200

    adding propriety

--- src/trainqlearning/experiments/record.py
+++ src/trainqlearning/experiments/record.py
@@ -3,15 +3,35 @@ import numpy as np
 
 class ExperimentRecord:
     def __init__(self):
-        self.transitions = []
+        self.episodes = []
+
+    @property
+    def all_transitions(self):
+        return [x for episode in self.episodes for x in episode["transitions"]]
+
+    @property
+    def rewards(self):
+        return [e["reward"] for e in self.episodes]
+
+    def set_new_episode(self, episode_id):
+        self.episodes.append({"episode_id": episode_id,
+                              "transitions": [],
+                              "reward": 0})
 
     def save_transition(self, transition):
-        self.transitions.append(transition)
+        self.episodes[-1]["transitions"].append(transition)
+        self.episodes[-1]["reward"] += transition.reward
 
     def get_last_transitions(self, size=10):
-        m = min(size, len(self.transitions))
-        return self.transitions[-m:]
+        m = min(size, len(self.all_transitions))
+        return self.all_transitions[-m:]
 
     def get_some_transitions(self, size=10):
-        return np.random.choice(self.transitions,
-                                size=min(size, len(self.transitions)))
+        return np.random.choice(self.all_transitions,
+                                size=min(size, len(self.all_transitions)))
+
+    def __len__(self):
+        return len(self.all_transitions)
+
+    def __repr__(self):
+        return "\n".join([str(episode) for episode in self.episodes])
